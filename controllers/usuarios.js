a1101afdb2eac1ae777c1eabd8f75d06869da343
Antonio Perez <japv67@gmail.com>
2021-05-20 12:25:47 -0500

    Seccion 9 completa

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,51 +1,88 @@
-const {response, request} = require('express');
+const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
 
-const usuariosGet = (req = request, res = response) => {
-    const {id, c} = req.query;
+
+const Usuario = require('../models/usuario');
+
+
+
+const usuariosGet = async(req = request, res = response) => {
+
+    const { limite = 5, desde = 0 } = req.query;
+    const query = { estado: true };
+
+    const [ total, usuarios ] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+            .skip( Number( desde ) )
+            .limit(Number( limite ))
+    ]);
+
     res.json({
-        msg:'Get API - conrolador',
-        id,
-        c
+        total,
+        usuarios
     });
 }
 
-const usuariosPost = (req, res = response) => {
+const usuariosPost = async(req, res = response) => {
     
-    const {nombre, id} = req.body;
+    const { nombre, correo, password, rol } = req.body;
+    const usuario = new Usuario({ nombre, correo, password, rol });
+
+    // Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync( password, salt );
+
+    // Guardar en BD
+    await usuario.save();
 
     res.json({
-        msg:'Post API - conrolador',
-        nombre,
-        id
+        usuario
     });
 }
 
-const usuariosPut = (req, res = response) => {
-    const {id} = req.params;
-    res.json({
-        msg:'Put API - conrolador',
-        id
-    });
+const usuariosPut = async(req, res = response) => {
+
+    const { id } = req.params;
+    const { _id, password, google, correo, ...resto } = req.body;
+
+    if ( password ) {
+        // Encriptar la contraseña
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync( password, salt );
+    }
+
+    const usuario = await Usuario.findByIdAndUpdate( id, resto );
+
+    res.json(usuario);
 }
 
 const usuariosPatch = (req, res = response) => {
     res.json({
-        msg:'Patch API - conrolador'
+        msg: 'patch API - usuariosPatch'
     });
 }
 
-const usuariosDelete = (req, res = response) => {
-    const {id} = req.params;
-    res.json({
-        msg:'Delete API - conrolador',
-        id
-    });
+const usuariosDelete = async(req, res = response) => {
+
+    const { id } = req.params;
+
+    // Fisicamente lo borramos
+    // const usuario = await Usuario.findByIdAndDelete( id );
+
+    const usuario = await Usuario.findByIdAndUpdate( id, { estado: false } );
+
+
+    res.json(usuario);
 }
+
+
+
 
 module.exports = {
     usuariosGet,
     usuariosPost,
     usuariosPut,
     usuariosPatch,
-    usuariosDelete
+    usuariosDelete,
 }
--- models/server.js
+++ models/server.js
@@ -1,43 +1,56 @@
-
 const express = require('express');
 const cors = require('cors');
 
+const { dbConnection } = require('../database/config');
+
 class Server {
 
-    constructor(){
-        //Inicializacion
-        this.app = express();
+    constructor() {
+        this.app  = express();
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios';
 
-        //Middlewares
+        // Conectar a base de datos
+        this.conectarDB();
+
+        // Middlewares
         this.middlewares();
 
-        //Rutas
+        // Rutas de mi aplicación
         this.routes();
     }
 
-    middlewares(){
-        //CORS
-        this.app.use(cors());
-
-        //Lectura y parseo del body 
-        this.app.use(express.json());
-
-        //Directorio publico
-        this.app.use(express.static('public'));
+    async conectarDB() {
+        await dbConnection();
     }
 
-    routes(){
-        this.app.use(this.usuariosPath, require('../routes/usuarios'));
+
+    middlewares() {
+
+        // CORS
+        this.app.use( cors() );
+
+        // Lectura y parseo del body
+        this.app.use( express.json() );
+
+        // Directorio Público
+        this.app.use( express.static('public') );
+
     }
 
-    listen(){        
-        this.app.listen(this.port, () =>{
-            console.log('Servidor corriendo en puerto', this.port);
+    routes() {
+        this.app.use( this.usuariosPath, require('../routes/usuarios'));
+    }
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port );
         });
     }
 
 }
+
+
+
 
 module.exports = Server;
